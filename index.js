506871bf93923d5eadc44209328903402b554b80
lpolevoy <polevlyu000@gmail.com>
2018-09-20 12:37:09 -0700

    Second Commit
    I don't remember
    
    Added options, associated options with display

--- index.js
+++ index.js
@@ -1,43 +1,125 @@
 const stTemps = {"pastYear": {
-                         "2018-01-01":0.09, "2018-01-02":0.70, "2018-01-03":0.04, "2018-01-04":0.20,"2018-01-05":0.204, "2018-01-06":0.92, "2018-01-07":0.5, "2018-01-08":0.990, "2018-01-09":1.02, "2018-01-10":6.02,"2018-01-11":3.022, "2018-01-12":3.20,
+                         "2018-01-02":0.70, "2018-01-03":0.04, "2018-01-04":0.20,"2018-01-05":0.204, "2018-01-06":0.92, "2018-01-07":0.5, "2018-01-08":0.990, "2018-01-09":1.02, "2018-01-10":6.02,"2018-01-11":3.022, "2018-01-12":3.20,
                          "2018-02-01":1.09, "2018-02-02":8.70, "2018-02-03":3.04, "2018-02-04":3.20,"2018-02-05":5.20, "2018-02-06":5.40, "2018-02-07":2.02, "2018-02-08":9.20, "2018-02-09":1.02, "2018-02-10":6.02,"2018-02-11":0.022, "2018-02-12":3.20,
                          "2018-03-01":7.09, "2018-03-02":11.70, "2018-03-03":3.04, "2018-03-04":13.20,"2018-03-05":15.20, "2018-03-06":19.20, "2018-03-07":15.02, "2018-03-08":9.20, "2018-03-09":10.02, "2018-03-10":11.02,"2018-03-11":10.022, "2018-03-12":13.20,
                          "2018-04-01":11.09, "2018-04-02":12.70, "2018-04-03":13.04, "2018-04-04":10.20,"2018-04-05":15.20, "2018-04-06":12.20, "2018-04-07":15.45, "2018-04-08":16.20, "2018-04-09":17.02, "2018-04-10":16.02,"2018-04-11":14.022, "2018-04-12":13.20,
                          "2018-05-01":10.99, "2018-05-02":14.80, "2018-05-03":14.04, "2018-05-04":13.20,"2018-05-05":15.20, "2018-05-06":19.20, "2018-05-07":20.02, "2018-05-08":19.20, "2018-05-09":19.02, "2018-05-10":16.02,"2018-05-11":10.022, "2018-05-12":17.20,
-                         "2018-06-01":20.09, "2018-06-02":22.70, "2018-06-03":19.9, "2018-06-04":21.20,"2018-06-05":25.20, "2018-06-06":24.20, "2018-06-07":25.02, "2018-06-08":26.20, "2018-06-09":23.02, "2018-06-10":22.02,"2018-06-11":23.022, "2018-06-12":24.20,
+                         "2018-06-01":20.09, "2018-06-02":22.70, "2018-06-03":19.9, "2018-06-04":21.20,"2018-06-05":25.20, "2018-06-06":24.20, "2018-06-07":25.02, "2018-06-08":26.20, "2018-06-09":23.02, "2018-06-10":22.02,"2018-06-11":23.022, "2018-06-12":24.20
+                         },
+                  "pastMonth": {
+                         "2018-01-02":4.00, "2018-01-03":0.00, "2018-01-04":3.00, "2018-01-05":6.00, "2018-01-06":4.64, "2018-01-07":3.25, "2018-01-08":2.42, "2018-01-09":1.15, "2018-01-10":1.57, "2018-01-11":1.27, "2018-01-12":2.0,
+                         "2018-01-13":2.78, "2018-01-14":3.2, "2018-01-15":4.01, "2018-01-16":3.35, "2018-01-17":2.56, "2018-01-18":4.98, "2018-01-19":5.8,"2018-01-20":5.9, "2018-01-21":5.78, "2018-01-22":5.95, "2018-01-23":5.4,
+                         "2018-01-24":4.83, "2018-01-25":4.15, "2018-01-26":4.44, "2018-01-27":3.9, "2018-01-28":3.33, "2018-01-29":2.789, "2018-01-30":2.99, "2018-01-31":3.79
                          }
-                 // "pastMonth": {"2018-01-01":"01", "2018-01-02":"04", "2018-01-03":"00", "2018-01-05":"06", "2018-01-09":"01", "2018-01-11":"01", "2018-01-15":"02"}
                 }
 
-// const stTemps = {"temperatures":{"2017-12-31":13860.1363,"2018-01-01":13412.44,"2018-01-02":14740.7563,"2018-01-03":15134.6513,"2018-01-04":15155.2263,"2018-01-05":16937.1738,"2018-01-06":17135.8363,"2018-01-07":16178.495,"2018-01-08":14970.3575,"2018-01-09":14439.4738,"2018-01-10":14890.7225,
-// "2018-01-11":13287.26,"2018-01-12":13812.715,"2018-01-13":14188.785,"2018-01-14":13619.0288,"2018-01-15":13585.9013,"2018-01-16":11348.02,"2018-01-17":11141.2488,"2018-01-18":11250.6475,"2018-01-19":11514.925,"2018-01-20":12759.6413,"2018-01-21":11522.8588,"2018-01-22":10772.15,
-// "2018-01-23":10839.8263,"2018-01-24":11399.52,"2018-01-25":11137.2375,"2018-01-26":11090.0638,"2018-01-27":11407.1538,"2018-01-28":11694.4675,"2018-01-29":11158.3938,"2018-01-30":10034.9975,"2018-01-31":10166.5063,"2018-02-01":9052.5763,"2018-02-02":8827.63,"2018-02-03":9224.3913,
-// "2018-02-04":8186.6488,"2018-02-05":6914.26,"2018-02-06":7700.3863,"2018-02-07":7581.8038,"2018-02-08":8237.2363,"2018-02-09":8689.8388,"2018-02-10":8556.6125,"2018-02-11":8070.7963,"2018-02-12":8891.2125,"2018-02-13":8516.2438,"2018-02-14":9477.84,"2018-02-15":10016.4888,
-// "2018-02-16":10178.7125,"2018-02-17":11092.1475,"2018-02-18":10396.63,"2018-02-19":11159.7238,"2018-02-20":11228.2413,"2018-02-21":10456.1725,"2018-02-22":9830.4263,"2018-02-23":10149.4625,"2018-02-24":9682.3825,"2018-02-25":9586.46,"2018-02-26":10313.0825,"2018-02-27":10564.4188,
-// "2018-02-28":10309.6413,"2018-03-01":10907.59,"2018-03-02":11019.5213,"2018-03-03":11438.6513,"2018-03-04":11479.7313,"2018-03-05":11432.9825,"2018-03-06":10709.5275,"201s8-03-07":9906.8,"2018-03-08":9299.2838,"2018-03-09":9237.05,"2018-03-10":8787.1638,"2018-03-11":9532.7413,
-// "2018-03-12":9118.2713,"2018-03-13":9144.1475,"2018-03-14":8196.8975,"2018-03-15":8256.9938,"2018-03-16":8269.3275,"2018-03-17":7862.1088,"2018-03-18":8196.0225,"2018-03-19":8594.1913,"2018-03-20":8915.9038,"2018-03-21":8895.4,"2018-03-22":8712.8913,"2018-03-23":8918.7438,
-// "2018-03-24":8535.8938,"2018-03-25":8449.835,"2018-03-26":8138.3363,"2018-03-27":7790.1575,"2018-03-28":7937.205,"2018-03-29":7086.4875,"2018-03-30":6844.3213,"2018-03-31":6926.0175,"2018-04-01":6816.74}}
+var dataTypePrevious; // stores previous data type
+var dataType; // stores current data type option
 var arr = []; // new empty array
-var parsedData = parseData(stTemps); // parses json data into form suitible to create line chart (defined below) // stores parsed arr as variable
-var datesArr = [];
-for (var i in arr) {
-    datesArr.push(arr[i].date);
+var parsedData = null;
+
+injectTypes();
+processAndInsert();
+
+// enables select button when type option changes
+// d3.selectAll("#types").on('change', function(d) {
+//     var datatype = getDataType();
+//     if (datatype == dataTypePrevious) {
+//         d3.select('button').attr('disabled', true);
+//     } else {
+//         d3.select('button').attr('disabled', null);
+//     }
+// });
+
+// reaction to user click of select button
+d3.selectAll('#types')
+    .on('change', function(d) {
+      processAndInsert();
+    });
+
+// on change of type, decide whether to gray out button or not
+// function processType() {
+//     var datatype = getDataType();
+//     if (datatype == dataTypePrevious) {
+//         d3.select('button').attr('disabled', true);
+//     } else {
+//         d3.select('button').attr('disabled', null);
+//     }
+// }
+
+// runs main methods needed to graph data
+function processAndInsert () {
+    // d3.selectAll('g').remove();
+
+    resetDataType();
+
+    parsedData = parseData(dataType); // parses json data into form suitible to create line chart (defined below) // stores parsed arr as variable
+    // console.log(arr);
+    // console.log(parsedData);
+    var datesArr = [];
+    for (var i in arr) {
+        datesArr.push(arr[i].date);
+    }
+
+    drawChart(parsedData); // call to drawChart method (defined below)
+    // console.log(datesArr);
+    // d3.selectAll('button').attr('disabled', true);
 }
-console.log(datesArr);
-drawChart(parsedData); // call to drawChart method (defined below)
+
+// resets the type of data to be displayed based on user's option selection
+function resetDataType() {
+    var datatype = getDataType();
+    if (datatype == "Temperature (\xB0C)") {
+        dataType = stTemps.pastYear;
+        dataTypePrevious = datatype;
+        console.log(datatype);
+        console.log(dataType);
+        console.log(dataTypePrevious);
+    } else if (datatype == "Temperature (\xB0F)") {
+        dataType = stTemps.pastMonth;
+        dataTypePrevious = datatype;
+    } else if (datatype == "Temperature (K)") {
+        dataType = null;
+        dataTypePrevious = datatype;
+        console.log("data in kelvins would display here");
+    } else if (datatype == "Humidity (%)") {
+        dataType = null;
+        dataTypePrevious = datatype;
+        console.log("humidity data would display here");
+    } else {
+        console.log("Sorry! No graph for you");
+    }
+}
+
+// returns selected option for type of data of first graph
+function getDataType() {
+    return document.getElementById("types").value;
+}
+
+// puts data types into drop down menu so user can choose from later, default is Temp celcius
+function injectTypes() {
+    document.getElementById("types").defaultValue = "Temperature(\xB0F)";
+    // default value: "Show me: (Choose one)"
+    var typesArr = ["Temperature (\xB0C)", "Temperature (\xB0F)", "Temperature (K)", "Humidity (%)" ];
+    for (var i = 0; i < typesArr.length; i++) {
+        var type = document.createElement("option");
+        type.innerHTML = typesArr[i];
+        document.getElementById("types").appendChild(type);
+    }
+}
 
 /**
  * Parse data into key-value pairs
  * @param {object} data Object containing historical data of temperatures
  */
 
-function parseData(data) {
-    // var arr = []; // new empty array
-    for (var i in data.temperatures) { // for every temperatures in parsed data
+function parseData(dataType) {
+    arr = []; // new empty array
+    for (var i in dataType) { // for every temperatures in parsed data
         arr.push({
             date: new Date(i), //date ('key')
-            value: data.temperatures[i] //convert string to number (the + does)
-        });
+            value: dataType[i]
+        })
     }
     return arr; // return array with key:value pairs
 }
@@ -47,93 +129,68 @@ function parseData(data) {
  * @param {object} data Object containing historical data of temperatures
  */
 function drawChart(data) {
-var svgWidth = 600, svgHeight = 400;
-var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+    var svgWidth = 600, svgHeight = 400;
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3.select('svg')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    // d3.selectAll('svg').remove();
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    d3.selectAll('svg > *').remove();
 
-var x = d3.scaleTime() // scales time
-    .domain([new Date("December 31, 2017"), new Date("June 12, 2018")])
-    .range([0, width]);
+    var svg = d3.select('svg')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
 
-// var x = d3.scaleLinear() // scales time
-//     .domain(parsedData[0].date.toString(), parsedData[parsedData.length - 1].date.toString())
-//     .range([0, width]);
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// var x = d3.scaleLinear() // scales time
-//     .domain([new Date("January 1, 2018"), new Date("June 12, 2018")])
-//     .range([0, width]);
-    // .tickFormat(function(d) { return parsedData[d].date.getMonth(); });
+    var x = d3.scaleTime() // scales time
+        .domain([new Date("January 1, 2018"), new Date("June 30, 2018")])
+        .range([0, width]);
 
+    var y = d3.scaleLinear() // scales y-axis
+        .range([height, 0]);
 
+    var line = d3.line()
+        .x(function(d) { return x(d.date)}) // sets x value of temperatures point based on 'date' and scaled value of 'date'
+        .y(function(d) { return y(d.value)}) // sets y value of temperatures point point based on 'value' and scaled value of 'value'
 
-// var a = d3.scalePoint().domain(['Apples','Oranges','Pears','Plums']).range([0,width]);
+        x.domain(d3.extent(data, function(d) { return d.date })); // informs d3 of scope of data, extent returns min & max of dates
+        y.domain(d3.extent(data, function(d) { return d.value })); // returns min & max y-value
+        // mins & maxs then used to make scales properly
 
-var y = d3.scaleLinear() // scales y-axis
-    .range([height, 0]);
+    g.append("g") // append a child 'g' into parent 'g'
+        .attr("transform", "translate(0," + height + ")") // starts at bottom left of parent 'g'
+        .call(d3.axisBottom(x)); // creates x-axis in newly created 'g' element
 
-var line = d3.line()
-    .x(function(d) { return x(d.date)}) // sets x value of temperatures point based on 'date' and scaled value of 'date'
-    .y(function(d) { return y(d.value)}) // sets y value of temperatures point point based on 'value' and scaled value of 'value'
+    svg
+        .append("g")
+        .append("text")
+        .text("January") // can be retrieved from data instead of being harcoded
+        .style("font-size", 10)
+        .style("font-family", "sans-serif")
+        .attr("transform", "translate(35," + (svgHeight - 15) + ")");
 
-    x.domain(d3.extent(data, function(d) { return d.date })); // informs d3 of scope of data  // extent returns min & max of dates
-    y.domain(d3.extent(data, function(d) { return d.value })); // returns min & max y-value
-    // mins & maxs then used to make scales properly
+    // y-axis label
+    g.append("g")
+        .call(d3.axisLeft(y)) // creates y-axis
+        .append("text") // creates y-axis label
+        .attr("fill", "#000") // fill of text tag
+        .attr("transform", "rotate(-90)") // rotates label 90 degrees CC-wise
+        .attr("y", 6) // y position of label
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end") // aligns text at end of text tag
+        .text("Temperature (" + "\xB0" + "C)"); // text inside text tag
 
-// var xAxis = g.axis()
-//     .scale(xScale)
-//     .tickFormat(function(d) { return dataset[d].date.toString(); })
-//     .orient("bottom");
-
-g.append("g") // append a child 'g' into parent 'g'
-    .attr("transform", "translate(0," + height + ")") // starts at bottom left of parent 'g'
-    .call(d3.axisBottom(x)); // creates x-axis in newly created 'g' element
-    // .tickFormat(function(d) { return parsedData[d].date.getMonth(); });
-
-//     .select(".domain") // why this?
-//     .remove();
-
-// var xAxis = d3.svg.axis()
-//         .scale(xScale)
-//         .orient("bottom");
-// // xAxis
-//     svg.append("g") // Add the X Axis
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(xAxis);
-//     //xAxis Label
-//     svg.append("text")
-//         .attr("transform", "translate(" + (w / 2) + " ," + (h + margin.bottom - 5) +")")
-//         .style("text-anchor", "middle")
-//         .text(function(d) {
-//             return d.date.toString();
-//         });
-
-// y-axis label
-g.append("g")
-    .call(d3.axisLeft(y)) // creates y-axis
-    .append("text") // creates y-axis label
-    .attr("fill", "#000") // fill of text tag
-    .attr("transform", "rotate(-90)") // rotates label 90 degrees CC-wise
-    .attr("y", 6) // y position of label
-    .attr("dy", "0.71em")
-    .attr("text-anchor", "end") // aligns text at end of text tag
-    .text("Temperature (K)"); // text inside text tag
-
-// creates line connecting data points
-g.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-width", 1.5)
-    .attr("d", line); // based on variable defined above, appends line for every data element
+    // creates line connecting data points
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("x", 10)
+        .attr("d", line); // based on variable defined above, appends line for every data element
 }
